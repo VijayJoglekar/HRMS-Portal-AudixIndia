f6444cc44b43734e0cfba3034e31c66d0dade4a4
Vijay <vijayjoglekar07@gmail.com>
2026-01-23 00:43:30 +0530

    Security Update: Move hardcoded secret to environment variables

--- hrms-backend/controllers/authController.js
+++ hrms-backend/controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const Employee = require("../models/Employee");
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, "secret123", { expiresIn: "1d" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 
 exports.registerUser = async (req, res) => {
